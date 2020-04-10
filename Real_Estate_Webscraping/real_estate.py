5c8198764f7a6daca39686889074ae6fbe4702ea
austin saleh <augustinesaleh@gmail.com>
2020-04-09 21:57:30 -0400

    Update to webscrapping project

--- Real_Estate_Webscraping/real_estate.py
+++ Real_Estate_Webscraping/real_estate.py
@@ -1,11 +1,14 @@
-
-
-
-
+import requests
+from bs4 import BeautifulSoup
 
 r = requests.get("http://www.pyclass.com/example.html", headers={'User-agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:61.0) Gecko/20100101 Firefox/61.0'})
 
 
+c = r.content
+
+soup = BeautifulSoup(c, "html.parser")
+
+all =soup.find_all("div",{"class":"cities"})
 
 
 
